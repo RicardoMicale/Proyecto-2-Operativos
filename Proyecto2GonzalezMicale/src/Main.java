5a612c04b20b292db4b8fd124ad1d6543d357e43
Carlos Gonzalez <carlosigonz@gmail.com>
2021-06-09 16:34:57 -0400

    Borrados comments

--- Proyecto2GonzalezMicale/src/Main.java
+++ Proyecto2GonzalezMicale/src/Main.java
@@ -1,20 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author ricar
+ * @author Ricardo Micale
+ *         Carlos Gonzalez
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) {s
     }
     
 }
