555307b61853eba905fe5cfb310f2bc06a130382
Ricardo Micale <ricardomicale@gmail.com>
2021-06-23 18:03:31 -0400

    cambio interfaz

--- Proyecto2GonzalezMicale/src/Interfaz.java
+++ Proyecto2GonzalezMicale/src/Interfaz.java
@@ -27,51 +27,20 @@ public class Interfaz extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        btnSalir = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
         labelCola1 = new javax.swing.JLabel();
         labelCola2 = new javax.swing.JLabel();
         labelCola3 = new javax.swing.JLabel();
-        labelRevision = new javax.swing.JLabel();
+        labelMantenimiento = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         idRevision = new javax.swing.JLabel();
-        btnSalir = new javax.swing.JButton();
-        labelMantenimiento = new javax.swing.JLabel();
+        labelRevision = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(102, 153, 0));
-
-        labelCola1.setFont(new java.awt.Font("Ebrima", 1, 14)); // NOI18N
-        labelCola1.setForeground(new java.awt.Color(51, 51, 51));
-        labelCola1.setText("Cola 1:");
-
-        labelCola2.setFont(new java.awt.Font("Ebrima", 1, 14)); // NOI18N
-        labelCola2.setForeground(new java.awt.Color(51, 51, 51));
-        labelCola2.setText("Cola 2:");
-
-        labelCola3.setFont(new java.awt.Font("Ebrima", 1, 14)); // NOI18N
-        labelCola3.setForeground(new java.awt.Color(51, 51, 51));
-        labelCola3.setText("Cola 3:");
-
-        labelRevision.setFont(new java.awt.Font("Ebrima", 1, 24)); // NOI18N
-        labelRevision.setForeground(new java.awt.Color(51, 51, 51));
-        labelRevision.setText("Pana en revision");
-
-        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
-
-        idRevision.setFont(new java.awt.Font("Ebrima", 1, 36)); // NOI18N
-        idRevision.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        idRevision.setText("1");
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(idRevision, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(idRevision, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 98, Short.MAX_VALUE)
-        );
+        jPanel1.setBackground(new java.awt.Color(204, 204, 204));
 
         btnSalir.setBackground(new java.awt.Color(255, 255, 255));
         btnSalir.setFont(new java.awt.Font("Ebrima", 1, 18)); // NOI18N
@@ -85,9 +54,75 @@ public class Interfaz extends javax.swing.JFrame {
             }
         });
 
+        jLabel1.setFont(new java.awt.Font("Ebrima", 1, 36)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel1.setText("Revision de Panas");
+
+        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
+
+        labelCola1.setFont(new java.awt.Font("Ebrima", 1, 14)); // NOI18N
+        labelCola1.setForeground(new java.awt.Color(51, 51, 51));
+        labelCola1.setText("Cola 1:");
+
+        labelCola2.setFont(new java.awt.Font("Ebrima", 1, 14)); // NOI18N
+        labelCola2.setForeground(new java.awt.Color(51, 51, 51));
+        labelCola2.setText("Cola 2:");
+
+        labelCola3.setFont(new java.awt.Font("Ebrima", 1, 14)); // NOI18N
+        labelCola3.setForeground(new java.awt.Color(51, 51, 51));
+        labelCola3.setText("Cola 3:");
+
         labelMantenimiento.setFont(new java.awt.Font("Ebrima", 1, 14)); // NOI18N
         labelMantenimiento.setForeground(new java.awt.Color(51, 51, 51));
-        labelMantenimiento.setText("Mantenimiento");
+        labelMantenimiento.setText("Mantenimiento:");
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(labelMantenimiento)
+                    .addComponent(labelCola3)
+                    .addComponent(labelCola2)
+                    .addComponent(labelCola1))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addComponent(labelCola1)
+                .addGap(53, 53, 53)
+                .addComponent(labelCola2)
+                .addGap(55, 55, 55)
+                .addComponent(labelCola3)
+                .addGap(53, 53, 53)
+                .addComponent(labelMantenimiento)
+                .addContainerGap(28, Short.MAX_VALUE))
+        );
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+
+        idRevision.setFont(new java.awt.Font("Ebrima", 1, 36)); // NOI18N
+        idRevision.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        idRevision.setText("1");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(idRevision, javax.swing.GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(idRevision, javax.swing.GroupLayout.DEFAULT_SIZE, 98, Short.MAX_VALUE)
+        );
+
+        labelRevision.setFont(new java.awt.Font("Ebrima", 1, 24)); // NOI18N
+        labelRevision.setForeground(new java.awt.Color(51, 51, 51));
+        labelRevision.setText("Pana en revision");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -95,43 +130,34 @@ public class Interfaz extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(38, 38, 38)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(labelCola1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 284, Short.MAX_VALUE)
-                        .addComponent(labelRevision))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(labelCola3)
-                            .addComponent(labelCola2)
-                            .addComponent(labelMantenimiento))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(btnSalir, javax.swing.GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE))))
-                .addGap(99, 99, 99))
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(112, 112, 112)
+                .addComponent(labelRevision)
+                .addGap(60, 60, 60)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(116, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(38, 38, 38)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(labelCola1)
-                    .addComponent(labelRevision))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(20, 20, 20)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(btnSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(19, 19, 19)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(11, 11, 11))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(labelCola2)
-                        .addGap(48, 48, 48)))
-                .addComponent(labelCola3)
-                .addGap(32, 32, 32)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnSalir, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(labelMantenimiento))
-                .addContainerGap(62, Short.MAX_VALUE))
+                        .addGap(34, 34, 34)
+                        .addComponent(labelRevision)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 90, Short.MAX_VALUE)
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -191,8 +217,10 @@ public class Interfaz extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnSalir;
     public static volatile javax.swing.JLabel idRevision;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     public static volatile javax.swing.JLabel labelCola1;
     public static volatile javax.swing.JLabel labelCola2;
     public static volatile javax.swing.JLabel labelCola3;
